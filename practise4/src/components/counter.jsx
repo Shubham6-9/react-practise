f67fe8625dd31f314d81a012af96313cf431aea1
Shubham6-9 <pujarashubham49@gmail.com>
2025-05-02 12:49:53 +0530

    add

--- practise4/src/components/counter.jsx
+++ practise4/src/components/counter.jsx
@@ -3,30 +3,30 @@ import React, { useEffect, useState } from 'react'
 function Counter({ target, des, icon }) {
     const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        setInterval(() => {
-            if (count < 1000) {
-                setCount(count + 50);
-                console.log(count);
-            } else {
-                clearInterval(setInterval)
-            }
-        }, 100)
-    }, [])
-
-    // let speed = +(target) / 100;
     // useEffect(() => {
     //     setInterval(() => {
-    //         setCount(count => {
-    //             if (count < target) {
-    //                 return count + speed;
-    //             } else {
-    //                 clearInterval(setInterval)
-    //                 return count;
-    //             }
-    //         })
-    //     }, 10)
+    //         if (count < 1000) {
+    //             setCount(count + 50);
+    //             console.log(count);
+    //         } else {
+    //             clearInterval(setInterval)
+    //         }
+    //     }, 100)
     // }, [])
+
+    let speed = +(target) / 100;
+    useEffect(() => {
+        setInterval(() => {
+            setCount(count => {
+                if (count < target) {
+                    return count + speed;
+                } else {
+                    clearInterval(setInterval)
+                    return count;
+                }
+            })
+        }, 10)
+    }, [])
 
     return (
         <div id="counter">
--- practise6/src/components/movie.jsx
+++ practise6/src/components/movie.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Movie(){
-    
+function Movie() {
+    const [err, setErr] = useState("")
+    const [result, setResult] = useState([])
 
-    return(
+    const search = (value) => {
+        if (value.length < 3) {
+            setErr("enter atleast 3 letter")
+            setResult([])
+        } else {
+            setErr("")
+            async function getting() {
+                let res = await fetch(`http://www.omdbapi.com/?s=${value}&apikey=b050125e`);
+                res = await res.json()
+                console.log(res);
+                if (res.search) {
+                    setResult(res.Search)
+                    return;
+                } else {
+                    setResult([])
+                }
+            }
+            getting()
+        }
+    }
+
+    return (
         <div>
-
+            <input type="text" className='border border-red-500' onChange={e => search(e.target.value)} />
+            <p className="text-red-600 text-[14px]">{err}</p>
+            <div>
+                {
+                    result.map((e, i) => {
+                        return <div key={i}>
+                            <p>{e}</p>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     )
 }
